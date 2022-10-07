a8e347edc45de53c83206d3550e6fbd5c6380c37
adeTunes <babatundeadebowale1998@gmail.com>
2022-10-07 17:06:10 +0100

    changes made to settings

--- chat_app/settings.py
+++ chat_app/settings.py
@@ -25,16 +25,18 @@ SECRET_KEY = 'django-insecure-op!&p)nx!47ig8_tg*e1o4az3j%*_k*%5we#4r@q%htgpfohqw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS =['*']
-CORS_ORIGIN_ALLOW_ALL = True
+# ALLOWED_HOSTS = []
+# CORS_ORIGIN_ALLOW_ALL = True
 
 
-# ALLOWED_HOSTS = ['http://localhost:5000']
+ALLOWED_HOSTS = ["*"]
 #
-# CORS_ORIGIN_ALLOW_ALL = False
-# CORS_ORIGIN_WHITELIST = (
-#     'http://localhost:5000',
-# )
+# CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ORIGIN_WHITELIST = [
+    'http://127.0.0.1:3004',
+    'http://localhost:3004'
+]
 
 
 # Application definition
@@ -55,13 +57,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'chat_app.urls'
@@ -152,6 +155,3 @@ PHONENUMBER_DB_FORMAT = 'INTERNATIONAL'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-
