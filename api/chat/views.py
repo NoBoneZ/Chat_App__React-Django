0ab55ed546a160d637c6b2239f78c50f3167621d
AbrahamAdekunle <107469961+AbrahamAdekunle@users.noreply.github.com>
2022-10-08 17:17:24 +0100

    added the conversations to user list and made message and convo detail readonly, and also validated the data in creation of conversation and messages

--- api/chat/views.py
+++ api/chat/views.py
@@ -5,7 +5,6 @@ from rest_framework.response import Response
 from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_200_OK
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, ListAPIView
 from django.db.models import Q
-from rest_framework.authtoken
 
 from .serializers import (ConversationSerializer, ConversationDetailSerializer,
                           MessagesSerializer, MessagesDetailSerializer,
